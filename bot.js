d7338b6fa6be33ae681eb9e3f750327b6ecbf1cf
denizozenc <39837176+denizozenc@users.noreply.github.com>
2018-07-19 13:33:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,7 +80,7 @@ client.unload = command => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('**Aleyküm selam** :gift: ');
+    msg.reply('**Aleyküm Selam Hoşgeldin Kardeşim** :yakyak:  ');
   }
   
 });
